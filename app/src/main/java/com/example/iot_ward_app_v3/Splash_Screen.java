0522b610a14de3c45ab7d9c7c2be48dca9927ff0
busyhanklux <23571329h@gmail.com>
2021-12-08 01:49:35 +0800

    2021/12/08 0149 1.02.07.02

--- app/src/main/java/com/example/iot_ward_app_v3/Splash_Screen.java
+++ app/src/main/java/com/example/iot_ward_app_v3/Splash_Screen.java
@@ -27,19 +27,16 @@ import java.util.Hashtable;
 public class Splash_Screen extends AppCompatActivity {
 
     String s_time = "";
-    int check_time_for_text_number;
-    long time_now = System.currentTimeMillis() / 1000;
+    int check_time_for_text;
+    //1需要被更動，2是改完了
 
-    TextView check_time_for_text;
+    long time_now = System.currentTimeMillis() / 1000;
 
     //把資料預載放這
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
-
-        check_time_for_text = findViewById(R.id.time_check);
-        check_time_for_text_number = Integer.parseInt((String) check_time_for_text.getText());
 
         /*
         File file = new File(getFilesDir(), "environment.txt"); //文字檔
@@ -61,7 +58,7 @@ public class Splash_Screen extends AppCompatActivity {
         if (!time_F.exists()) {
             try {
                 time_F.createNewFile();
-                check_time_for_text.setText("1");
+                check_time_for_text = 1;
                 s_time = s_time + time_now;
                 Toast hint = Toast.makeText(Splash_Screen.this, s_time,Toast.LENGTH_SHORT);
                 hint.show();
@@ -79,28 +76,41 @@ public class Splash_Screen extends AppCompatActivity {
                 long change_time = dataSnapshot.getValue(long.class);
                 //改動他
                 if(change_time >= time_now){
-                    Toast hint = Toast.makeText(Splash_Screen.this, "變動",Toast.LENGTH_SHORT);
-                    hint.show();
                     s_time = s_time + time_now;
-                    check_time_for_text.setText("1");
-
+                    check_time_for_text = 1;
                 }
+
                 //不改他
                 if(change_time < time_now){
                     Toast hint = Toast.makeText(Splash_Screen.this, "沒有變動",Toast.LENGTH_SHORT);
                     hint.show();
-                    check_time_for_text.setText("2");
+                    check_time_for_text = 2;
 
                 }
+
+                if(check_time_for_text == 1)
+                {
+                    Toast hint = Toast.makeText(Splash_Screen.this, "他需要被變動",Toast.LENGTH_SHORT);
+                    hint.show();
+                    Intent splash_to_main = new Intent();
+                    splash_to_main.setClass(Splash_Screen.this,MainActivity.class);
+                    startActivity(splash_to_main);
+                    finish();
+                }
+
+                //測試數字，如果他有變動就是1
+                //Toast hint2 = Toast.makeText(Splash_Screen.this, check_time_for_text.getText() ,Toast.LENGTH_SHORT);
+                //hint2.show();
             }
             @Override
             public void onCancelled(DatabaseError error) { }
             });
 
-        Toast hint2 = Toast.makeText(Splash_Screen.this, check_time_for_text.getText() ,Toast.LENGTH_SHORT);
-        hint2.show();
+        //測試數字，這邊不會被void onDataChange改動，所以...
+        //Toast hint2 = Toast.makeText(Splash_Screen.this, check_time_for_text.getText() ,Toast.LENGTH_SHORT);
+        //hint2.show();
 
-        if(check_time_for_text.getText() == "1")
+        if(check_time_for_text == 1)
         {
             FileOutputStream fos = null;
             try {
@@ -109,7 +119,7 @@ public class Splash_Screen extends AppCompatActivity {
                 fos.write(s_time.getBytes());
                 //fos關閉(結尾時必要)
                 fos.close();
-                check_time_for_text.setText("2");
+                check_time_for_text = 2;
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -148,8 +158,8 @@ public class Splash_Screen extends AppCompatActivity {
         }
          */
 
-        //檢查完畢後(全部為0)，傳送
-        if(check_time_for_text.getText() == "2"){
+        //檢查完畢後(2)，傳送
+        if(check_time_for_text == 2){
             Intent splash_to_main = new Intent();
             splash_to_main.setClass(Splash_Screen.this,MainActivity.class);
             startActivity(splash_to_main);
